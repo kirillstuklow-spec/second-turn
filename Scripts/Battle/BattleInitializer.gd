ba57eb0b3166ffc451574fef554583e35f7d7e11
kirillstuklow-spec <kirillstuklow@gmail.com>
2026-07-20 23:34:33 +0300

    Update BattleInitializer.gd

--- Scripts/Battle/BattleInitializer.gd
+++ Scripts/Battle/BattleInitializer.gd
@@ -3,20 +3,55 @@ extends Node
 class_name BattleInitializer
 
 
+# ============================================================
+# ИНИЦИАЛИЗАЦИЯ ТЕСТОВОГО БОЯ
+# ============================================================
+
 func initialize_test_battle(
 	battle_state: BattleState,
-	archer_data: UnitData,
+	first_player_unit_data: UnitData,
+	second_player_unit_data: UnitData,
 	defense_dummy_data: UnitData,
 	armor_dummy_data: UnitData
 ) -> void:
-	print("BattleInitializer: initialize_test_battle called")
+	print(
+		"BattleInitializer: initialize_test_battle called"
+	)
 
-	var archer : UnitRuntime = battle_state.spawn_unit(archer_data, 1, 0, 2)
+	var first_player_unit: UnitRuntime = (
+		battle_state.spawn_unit(
+			first_player_unit_data,
+			1,
+			0,
+			2
+		)
+	)
 
-	battle_state.spawn_unit(defense_dummy_data, 2, 6, 2)
-	battle_state.spawn_unit(armor_dummy_data, 2, 6, 3)
+	battle_state.spawn_unit(
+		second_player_unit_data,
+		1,
+		0,
+		3
+	)
 
-	if archer != null:
-		battle_state.set_active_unit(archer)
+	battle_state.spawn_unit(
+		defense_dummy_data,
+		2,
+		6,
+		2
+	)
+
+	battle_state.spawn_unit(
+		armor_dummy_data,
+		2,
+		6,
+		3
+	)
+
+	if first_player_unit != null:
+		battle_state.set_active_unit(
+			first_player_unit
+		)
 
 	battle_state.print_units()
+	
