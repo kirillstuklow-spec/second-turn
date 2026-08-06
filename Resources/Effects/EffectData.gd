dad93293ef308b1f84668d44a80b6e0740a1c610
kirillstuklow-spec <kirillstuklow@gmail.com>
2026-08-06 22:46:18 +0300

    Update EffectData.gd

--- Resources/Effects/EffectData.gd
+++ Resources/Effects/EffectData.gd
@@ -2,6 +2,24 @@ extends Resource
 
 class_name EffectData
 
+
+enum ReapplyPolicy {
+	REFRESH,
+	IGNORE,
+	REPLACE
+}
+
+
+@export var effect_id : String = ""
+
 @export var effect_name : String = ""
 
 @export_multiline var description : String = ""
+
+@export var reapply_policy : ReapplyPolicy = ReapplyPolicy.REFRESH
+
+@export var duration : EffectDurationData = null
+
+@export var triggers : Array[EffectTriggerData] = []
+
+@export var passive_rules : Array[PassiveRuleData] = []
